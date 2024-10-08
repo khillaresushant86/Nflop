c87a28b6180b2f67eea7cc671a8ea086e0062977
Alexander Koval <kovalidis@gmail.com>
2024-10-09 00:25:30 +0300

    Example of crash for help command

--- src/main/cli/cli.c
+++ src/main/cli/cli.c
@@ -6612,7 +6612,7 @@ typedef struct {
 }
 #endif
 
-static void cliHelp(const char *cmdName, char *cmdline);
+STATIC_UNIT_TESTED void cliHelp(const char *cmdName, char *cmdline);
 
 // should be sorted a..z for bsearch()
 const clicmd_t cmdTable[] = {
@@ -6773,7 +6773,7 @@ const clicmd_t cmdTable[] = {
 #endif
 };
 
-static void cliHelp(const char *cmdName, char *cmdline)
+STATIC_UNIT_TESTED void cliHelp(const char *cmdName, char *cmdline)
 {
     bool anyMatches = false;
 
